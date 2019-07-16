559bd83509fc55faf7d9f4cbed7e8e156c5cbfe6
Unknown <wesleyaegaz@gmail.com>
2019-07-15 22:18:00 -0400

    First half of the pseudocode and project setup

---
+++ src/KapustaSestina.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : Sestina.cpp
+// Author      : Wes Holman
+// Version     : 0.1
+// Copyright   : Copyright 2019
+// Description : Generates sestinas out of .txt source material
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!WES HOLMAN'S SESTINA GENERATOR!!!" << endl; // prints !!!Hello World!!!
+	//Opens and reads stop words, storing them as set
+	//hardcoded path to ebook
+	//tally non-stopwords occuring at end of clauses, store in vector
+	//set capacity of additional vector to size of current
+	//transfer all entries with 6 or more into it
+	//Sort by size, print out top 20 entries
+	//randomly pick 6, save into String array
+	//
+	return 0;
+}
